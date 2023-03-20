5a316e76f9a4f59d70e7eabc50bf935847cea313
elkirko <kirkhagglund@gmail.com>
2023-03-19 18:36:12 -0600

    testing post routes

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 const { connect, connection, connections } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetwork';
+/*const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetwork';
 
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+});*/
 
-/*connect('mongodb://localhost/socialNetwork', {
+connect('mongodb://localhost/socialNetwork', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});*/
+});
 
 module.exports = connection;
